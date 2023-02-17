a2b87d1cb7d65802936cf69464d64986ea487ca1
jev <jev@DESKTOP-S1A7OO2>
2023-02-17 16:44:04 +0300

    Add project files.

---
+++ school/Switchcaseimage.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace school
+{
+    partial class Switchcaseimage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arka Plan Rengi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bileşen Resmi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Açıklama";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(631, 396);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(543, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Orjinal",
+            "Kırmızı",
+            "Yeşil",
+            "Mor",
+            "Mavi"});
+            this.comboBox1.Location = new System.Drawing.Point(120, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "İşlemci",
+            "Bellek",
+            "Ekran kartı",
+            "Anakart",
+            "Kasa",
+            "Klavye",
+            "",
+            "Fare",
+            "Harddisk"});
+            this.comboBox2.Location = new System.Drawing.Point(347, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Switchcaseimage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Switchcaseimage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Switchcaseimage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ school/Switchcaseimage.cs
@@ -0,0 +1,102 @@
+﻿using school.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace school
+{
+    public partial class Switchcaseimage : Form
+    {
+        Color orjinal;
+        public Switchcaseimage()
+        {
+            InitializeComponent();
+        }
+
+        private void Switchcaseimage_Load(object sender, EventArgs e)
+        {
+            orjinal = this.BackColor;
+            pictureBox1.Image = Resources.yok;
+            pictureBox1.SizeMode=PictureBoxSizeMode.StretchImage;
+            comboBox1.SelectedItem = "Orjinal";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem)
+            {
+                case "Mor":
+                    BackColor = Color.Purple;
+                    break;
+                case "Mavi":
+                    BackColor = Color.Blue;
+                    break;
+                case "Yeşil":
+                    BackColor = Color.Green;
+                    break;
+                case "Kırmızı":
+                    BackColor = Color.Red;
+                    break;
+                case "Orjinal":
+                    this.BackColor= orjinal;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*
+             İşlemci
+             Bellek
+             Ekran kartı
+             Anakart
+             Kasa
+             Klavye
+             Fare
+             */
+            switch (comboBox2.SelectedItem)
+            {
+                case "İşlemci":
+                    textBox1.Text = "İşlemci";
+                    pictureBox1.Image = Resources.islemci;
+                    break;
+                case "Bellek":
+                    textBox1.Text = "Bellek";
+                    pictureBox1.Image = Resources.ddr4;
+                    break;
+                case "Ekran kartı":
+                    textBox1.Text = "Ekran kartı";
+                    pictureBox1.Image = Resources.asus_geforce;
+                    break;
+                case "Anakart":
+                    textBox1.Text = "Anakart";
+                    pictureBox1.Image = Resources.anakart;
+                    break;
+                case "Kasa":
+                    textBox1.Text = "Kasa";
+                    pictureBox1.Image= Resources.kasa;
+                    break;
+                case "Klavye":
+                    textBox1.Text = "Klavye";
+                    pictureBox1.Image= Resources.klavye;
+                    break;
+                case "Fare":
+                    textBox1.Text = "Fare";
+                    pictureBox1.Image= Resources.mouse;
+                    break;
+                default:
+                    textBox1.Text = "Ürün Bulunamadı";
+                    pictureBox1.Image= Resources.yok;
+                    break;
+            }
+        }
+    }
+}
